501f6ec5c6b723ad92b424c848b9807268695145
Shane <143796207+Shane0821@users.noreply.github.com>
2024-02-11 14:31:26 +0800

    add zobrist hash

---
+++ src/hash.cpp
@@ -0,0 +1,25 @@
+#include "hash.h"
+
+#include <ctime>
+#include <random>
+
+Zobrist::Zobrist() {
+    for (int i = 0; i < 2; i++) {
+        for (int x = 0; x < Board::BOARD_SIZE; x++) {
+            for (int y = 0; y < Board::BOARD_SIZE; y++) {
+                m_hashTable[i][x][y] = generateRandomNumber();
+            }
+        }
+    }
+    m_boardHash = generateRandomNumber();
+}
+
+unsigned long long Zobrist::generateRandomNumber() {
+    static std::mt19937 mt(time(NULL));
+    static std::uniform_int_distribution<unsigned long long> dist(0, UINT64_MAX);
+    return dist(mt);
+}
+
+void Zobrist::update(int x, int y, Board::CHESS_COLOR color) {
+    m_boardHash ^= m_hashTable[color][x][y];
+}
---
+++ src/hash.h
@@ -0,0 +1,21 @@
+#ifndef HASH_H
+#define HASH_H
+
+#include "board.h"
+
+class Zobrist {
+   public:
+    Zobrist();
+
+    static unsigned long long generateRandomNumber();
+
+    void update(int x, int y, Board::CHESS_COLOR);
+
+    unsigned long long getBoardHash() { return m_boardHash; }
+
+   private:
+    unsigned long long m_hashTable[2][Board::BOARD_SIZE][Board::BOARD_SIZE];
+    unsigned long long m_boardHash;
+};
+
+#endif
