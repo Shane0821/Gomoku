a3bf9d6f5d28bce9308bd77798dfd4fea2914c6f
Shane <143796207+Shane0821@users.noreply.github.com>
2024-02-15 10:18:18 +0800

    update timer

--- src/timer.h
+++ src/timer.h
@@ -1,22 +1,25 @@
 #ifndef TIMER_H
 #define TIMER_H
 
-#include <ctime>
+#include <chrono>
 
 class Timer {
    public:
-    Timer() { m_preTime = clock(); }
+    Timer() { m_preTime = std::chrono::high_resolution_clock::now(); }
 
-    void recordCurrent() { m_preTime = clock(); }
+    void recordCurrent() { m_preTime = std::chrono::high_resolution_clock::now(); }
 
     int getTimePass() const {
-        return 1.0 * (clock() - m_preTime) / CLOCKS_PER_SEC * 1000;
+        auto now = std::chrono::high_resolution_clock::now();
+        auto duration =
+            std::chrono::duration_cast<std::chrono::milliseconds>(now - m_preTime);
+        return duration.count();
     }
 
     const static int TIME_OUT = __INT32_MAX__;
 
    private:
-    time_t m_preTime;
+    std::chrono::high_resolution_clock::time_point m_preTime;
 };
 
 #endif
