f0e23d3abed55ec6f464b0a9aadb5f2f1a544c3f
Shane <143796207+Shane0821@users.noreply.github.com>
2024-02-18 19:51:19 +0800

    add docs for zobrist hash

--- src/hash.h
+++ src/hash.h
@@ -3,19 +3,42 @@
 
 #include "board.h"
 
+/**
+ * @class Zobrist
+ * @brief Class for generating and updating Zobrist hash values for a game board.
+ */
 class Zobrist {
    public:
-    Zobrist();
-
+    /**
+     * @brief Generates a random number for Zobrist hashing.
+     * @return The generated random number.
+     */
     static unsigned long long generateRandomNumber();
 
-    void update(int x, int y, Board::PIECE_COLOR);
+    /**
+     * @brief Constructs a Zobrist object.
+     */
+    Zobrist();
 
+    /**
+     * @brief Updates the Zobrist hash value based on the given move.
+     * @param x The x-coordinate of the move.
+     * @param y The y-coordinate of the move.
+     * @param color The color of the piece placed on the board.
+     */
+    void update(int x, int y, Board::PIECE_COLOR color);
+
+    /**
+     * @brief Gets the current Zobrist hash value of the game board.
+     * @return The Zobrist hash value.
+     */
     unsigned long long getBoardHash() const { return m_boardHash; }
 
    private:
-    unsigned long long m_hashTable[2][Board::BOARD_SIZE][Board::BOARD_SIZE];
-    unsigned long long m_boardHash;
+    unsigned long long
+        m_hashTable[2][Board::BOARD_SIZE]
+                   [Board::BOARD_SIZE];  // Hash table for storing random numbers
+    unsigned long long m_boardHash;      // Current Zobrist hash value of the game board
 };
 
 #endif
