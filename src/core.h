aed1bdcdf5fc86e7539084a269e6e00ed4ece7bd
Shane <143796207+Shane0821@users.noreply.github.com>
2024-02-20 15:58:22 +0800

    upd time limit

--- src/core.h
+++ src/core.h
@@ -28,7 +28,7 @@ class Core {
     static bool ITERATIVE_DEEPENING;
     static int BRANCH_FACTOR;
     const static int INF = __INT32_MAX__ - 100;
-    const static int TIME_LIMIT = 5960;
+    const static int TIME_LIMIT = 5900;
 
    private:
     int negMiniMaxSearch(int depth, Board::PIECE_COLOR player, int alpha, int beta);
