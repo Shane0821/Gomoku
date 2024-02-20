bf8a3eb476897c838a1239629be7b138eec75b01
Shane <143796207+Shane0821@users.noreply.github.com>
2024-02-20 12:58:31 +0800

    add arg for determing judger mode

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #pragma GCC optimize("O3")
 
+#include <cstring>
+
 #include "judger.h"
 
-int main() {
+int main(int argc, char* argv[]) {
     Judger judger;
-    // Judger::JUDGER_MODE = Judger::MODE::ONLINE_JUDGE;
+
+    if (argc > 1 && std::strcmp(argv[1], "json") == 0) {
+        Judger::JUDGER_MODE = Judger::MODE::ONLINE_JUDGE;
+    }
     judger.startGame();
     return 0;
 }
